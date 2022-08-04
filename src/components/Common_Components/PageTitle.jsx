fa86a2e497a6c561f4ad7ce9cb91f0a89567952f
AbhishekChorotiya <2020kuec2033@iiitkota.ac.in>
2022-08-05 02:32:07 +0530

    profileImageIssueSolved

--- src/components/Common_Components/PageTitle.jsx
+++ src/components/Common_Components/PageTitle.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 
-
 const PageTitle = (props) => {
     return (
-        <div className="block-heading pt-0 mb-2" id='page_title'>
+        <div className="block-heading pt-0 mb-2" >
     
-            <h2 className="d-flex justify-content-center" >{props.title}</h2>
+            <h2 className="d-flex justify-content-center" style={{color:'#e1775d',fontFamily:'play',fontSize:'2.5rem'}}>{props.title}</h2>
             <p className= "d-flex justify-content-center">{props.desc}</p>
         </div>
     )
