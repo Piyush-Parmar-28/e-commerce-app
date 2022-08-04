c1a1828f40b06d0d6e8726f4dc8ef4c990536c95
AbhishekChorotiya <2020kuec2033@iiitkota.ac.in>
2022-08-04 20:33:15 +0530

    responsiveNavbar

--- src/components/Common_Components/Navbar2.jsx
+++ src/components/Common_Components/Navbar2.jsx
@@ -35,14 +35,14 @@ export default function Navbar2(props) {
     return (
         <div className="my-navbar d-flex justify-content-between align-items-center" >
 
-            <div>
+            <div id='logo'>
                 <Link to="/home">
                     <h5 className="text-black">Sliver Store</h5>
                 </Link>
             </div>
 
-            <div className='d-flex'>
-                <input className="me-2" type="text" placeholder="Search anything..." name='item' onChange={getSearchText} />
+            <div className='d-flex' id='search_input' >
+                <input id='search_input'  className="me-2" type="text" placeholder="Search anything..." name='item' onChange={getSearchText} />
 
                 <Link to={"/products?item=" + textData}>
                     <button className="btn-icon btn-xsmall" type="submit">
@@ -56,9 +56,9 @@ export default function Navbar2(props) {
                 {/* Do not use 'href' to redirect page in react as it will refresh page every time we redirect the page. */}
                 {/* Use 'Link' instead as it will not refresh our page on redirect. */}
                 <div className='d-flex justify-content-center align-items-center me-2'>
-                    <label className='me-2'><b>Cart</b></label>
+                    <label className='me-2' id='cart'><b>Cart</b></label>
 
-                    <Link to="/cart">
+                    <Link to="/cart" id='cart_icon'>
                         <p className='badge'>{totalItems}</p>
 
                         <button type="button" className="btn-icon btn-xsmall me-2">
@@ -68,17 +68,17 @@ export default function Navbar2(props) {
                 </div>
 
                 <div className='d-flex justify-content-center align-items-center ms-2'>
-                    <label><b>Profile</b></label>
+                    <label id="profile"><b>Profile</b></label>
 
                     <Link to="/profile">
-                        <button type="button" className="btn-icon btn-xsmall me-2">
+                        <button type="button" className="btn-icon btn-xsmall me-2"  id='profile_icon'>
                             <AccountCircleOutlinedIcon />
                         </button>
                     </Link>
                 </div>
 
-                <div className='ms-2'>
-                    {!status && <Link to="/">
+                <div className='ms-2' id='logout_icon'>
+                    {!status && <Link to="/" >
                         <button type="button" className="btn-icon btn-xsmall btn-bg-green me-2">
                             <LoginIcon />
                         </button>
--- src/newComponents/productsPage/card/card.jsx
+++ src/newComponents/productsPage/card/card.jsx
@@ -1,39 +1,48 @@
 import React from "react";
-import style from './card.module.css';
+import style from "./card.module.css";
 import Image from "../../../pages/Image";
+import { Link } from "react-router-dom";
 
-import AddIcon from '@mui/icons-material/Add';
+import AddIcon from "@mui/icons-material/Add";
 import { Rating } from "@mui/material";
 
 const Card = (props) => {
-
-    return (
-        <div className={style.card}>
-            <div className={style.image}>
-                <Image photoID={props.imageID}></Image>
-            </div>
-
-            <div className={style.detail_div}>
-                <div className={style.name_div}>
-                    <a>{props.name}</a>
-                </div>
-
-                <div className={style.price_div}>
-                    <span className={style.ratings}>
-                        <Rating name="read-only" size="small" precision={0.5} value={props.ratings} readOnly />
-                    </span>
-                    <span className={style.price}> Rs. {props.price}</span>
-                </div>
-
-                <div className="d-flex justify-content-between mt-1">
-                    <button className="btn-normal"> Details</button>
-                    <button type="submit" className="btn-icon btn-bg-green btn-small">
-                        <AddIcon />
-                    </button>
-                </div>
-            </div>
+  return (
+    <div className={style.card}>
+      <Link to={"/selected?selectedProduct=" + props.productID}>
+        <div className={style.image}>
+          <Image photoID={props.imageID}></Image>
         </div>
-    );
-}
+      </Link>
+      <div className={style.detail_div}>
+        <div className={style.name_div}>
+          <a>{props.name}</a>
+        </div>
+
+        <div className={style.price_div}>
+          <span className={style.ratings}>
+            <Rating
+              name="read-only"
+              size="small"
+              precision={0.5}
+              value={props.ratings}
+              readOnly
+            />
+          </span>
+          <span className={style.price}> Rs. {props.price}</span>
+        </div>
+
+        <div className="d-flex justify-content-between mt-1">
+          <Link to={"/selected?selectedProduct=" + props.productID}>
+            <button className="btn-normal"> Details</button>
+          </Link>
+          <button type="submit" className="btn-icon btn-bg-green btn-small">
+            <AddIcon />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Card;
