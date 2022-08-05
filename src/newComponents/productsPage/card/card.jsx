ef0866f31e84d09fed82371a02a19f5bf96be589
Piyush Parmar <2822piyushparmar@gmail.com>
2022-08-05 14:49:29 +0530

    Created Additional Products Page

--- src/newComponents/productsPage/card/card.jsx
+++ src/newComponents/productsPage/card/card.jsx
@@ -1,93 +1,90 @@
-import React,{useEffect,useState} from "react";
+import React, { useEffect, useState } from "react";
 import style from "./card.module.css";
 import Image from "../../../pages/Image";
 import { useNavigate, Link } from 'react-router-dom';
-
 
 import AddIcon from "@mui/icons-material/Add";
 import { Rating } from "@mui/material";
 
 const Card = (props) => {
-  const navigate= useNavigate()
+    const navigate = useNavigate()
 
-  const [product, setProduct]= useState({
-    productID: "",
-    itemPrice: ""
-})
-
-useEffect(() =>{
-    setProduct( {productID: props.productID, itemPrice: props.price} )
-}, [])
-
-const postData= async (event)=>{
-    event.preventDefault()
-    // Using object destructuring for: productID= product.productID
-    const {productID, itemPrice}= product
-
-    const res= await fetch("/AddToCart", {
-        method: "post",
-        headers: {
-            "Content-Type": "application/json"
-        },
-
-        // Converting JSON to string since our backend cannot understand JSON
-        //  The string will be sent as body
-        body: JSON.stringify({
-            //  Uisng object destructuring for productID= productID
-            productID, itemPrice
-        })
+    const [product, setProduct] = useState({
+        productID: "",
+        itemPrice: ""
     })
 
-    const data= await res.json()
+    useEffect(() => {
+        setProduct({ productID: props.productID, itemPrice: props.price })
+    }, [])
 
-    if(data.status === 200){
-        navigate("/cart")
+    const postData = async (event) => {
+        event.preventDefault()
+        // Using object destructuring for: productID= product.productID
+        const { productID, itemPrice } = product
+
+        const res = await fetch("/AddToCart", {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json"
+            },
+
+            // Converting JSON to string since our backend cannot understand JSON
+            //  The string will be sent as body
+            body: JSON.stringify({
+                //  Uisng object destructuring for productID= productID
+                productID, itemPrice
+            })
+        })
+
+        const data = await res.json()
+
+        if (data.status === 200) {
+            navigate("/cart")
+        }
+        else {
+            window.alert("Failed to add product to cart!\nTry Again.")
+        }
     }
-    else{
-        window.alert("Failed to add product to cart!\nTry Again.")
-    }
-}
 
+    return (
+        <div className={style.card}>
+            <Link to={"/selected?selectedProduct=" + props.productID}>
+                <div className={style.image}>
+                    <Image photoID={props.imageID}></Image>
+                </div>
+            </Link>
+            <div className={style.detail_div}>
+                <div className="mt-2 text-center">
+                    <h5>{props.name}</h5>
+                </div>
 
-  
-  return (
-    <div className={style.card}>
-      <Link to={"/selected?selectedProduct=" + props.productID}>
-        <div className={style.image}>
-          <Image photoID={props.imageID}></Image>
-        </div>
-      </Link>
-      <div className={style.detail_div}>
-        <div className={style.name_div}>
-          <a>{props.name}</a>
-        </div>
+                <div className={style.price_div}>
+                    <span className={style.ratings}>
+                        <Rating
+                            name="read-only"
+                            size="small"
+                            precision={0.5}
+                            value={props.ratings}
+                            readOnly
+                        />
+                    </span>
+                    <span className={style.price}> Rs. {props.price}</span>
+                </div>
 
-        <div className={style.price_div}>
-          <span className={style.ratings}>
-            <Rating
-              name="read-only"
-              size="small"
-              precision={0.5}
-              value={props.ratings}
-              readOnly
-            />
-          </span>
-          <span className={style.price}> Rs. {props.price}</span>
+                <div className="d-flex justify-content-between mt-1">
+                    <Link to={"/selected?selectedProduct=" + props.productID}>
+                        <button className="btn-normal btn-xsmall"> Details</button>
+                    </Link>
+                    <form method="post">
+                        <button type="submit" className="btn-icon btn-bg-green btn-xsmall" onClick={postData}>
+                            <AddIcon />
+                        </button>
+                    </form>
+                </div>
+            </div>
         </div>
-
-        <div className="d-flex justify-content-between mt-1">
-          <Link to={"/selected?selectedProduct=" + props.productID}>
-            <button className="btn-normal"> Details</button>
-          </Link>
-          <form method="post">
-          <button type="submit" className="btn-icon btn-bg-green btn-small" onClick={postData}>
-            <AddIcon />
-          </button>
-          </form>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Card;
--- src/newComponents/productsPage/products.jsx
+++ src/newComponents/productsPage/products.jsx
@@ -1,37 +1,45 @@
-import React, { Fragment,useState,useEffect } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import style from "./products.module.css";
 import Card from "./card/card";
 
 
 const Productss = () => {
 
-   const [products, setProducts] = useState([])
-   if(window.location.search == '') window.location.href ='/';
-   useEffect(() => {
-       fetch(`SearchProducts${window.location.search.toLowerCase()}`).then(data => data.json()).then(myData => {
-           setProducts(myData)
-       })
-   }, [])
+    const [products, setProducts] = useState([])
+    if (window.location.search == '') window.location.href = '/';
+    
+    useEffect(() => {
+        fetch(`SearchProducts${window.location.search.toLowerCase()}`).then(data => data.json()).then(myData => {
+            setProducts(myData)
+        })
+    }, [])
 
-   const render = products.map((data)=>
-      <Card key={data._id} name={data.Product} productID={data._id} ratings={data.Ratings} price={data.Price} imageID={data.ImageID}></Card>
-   )
+    const render = products.map((data) =>
+        <Card 
+            key={data._id} 
+            name={data.Product} 
+            productID={data._id} 
+            ratings={data.Ratings} 
+            price={data.Price} 
+            imageID={data.ImageID}>
+        </Card>
+    )
 
 
-  return (
-    <Fragment>
-      <div className={style.outter_div}>
-        <div className={style.left_div}></div>
-        <div className={style.right_div}>
-          <div className={style.right_inner_div}>
+    return (
+        <Fragment>
+            <div className={style.outter_div}>
+                <div className={style.left_div}></div>
+                <div className={style.right_div}>
+                    <div className={style.right_inner_div}>
 
-            {render}
-   
-          </div>
-        </div>
-      </div>
-    </Fragment>
-  );
+                        {render}
+
+                    </div>
+                </div>
+            </div>
+        </Fragment>
+    );
 };
 
 export default Productss;
