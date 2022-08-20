ebfe8aee5f3025e916be4832a3bfd6b3a04a35a9
Piyush Parmar <2822piyushparmar@gmail.com>
2022-08-20 16:56:22 +0530

    Updated Category Hover

--- src/components/HomePage/Category_Image.jsx
+++ src/components/HomePage/Category_Image.jsx
@@ -4,11 +4,13 @@ import { Link } from "react-router-dom"
 const Category_Image = (props) => {
     return (
         <div className={props.display}>
-            <Link to={"/products?item="+ props.category}>
+            <Link to={"/products?item=" + props.category}>
                 <img className="img-fluid fill-image" src={props.image} alt="Image Loading" />
-                <span className="info">
-                    <h4 className="heading categoryHover" style={{padding:'1rem'}}>{props.label}</h4>
+
+                <span className="info px-5">
+                    <h4 className="heading categoryHover" style={{ padding: '0.7rem' }}>{props.label}</h4>
                 </span>
+
             </Link>
         </div>
     )
